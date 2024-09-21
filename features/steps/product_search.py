1ebb878de337fc05aa3ff445e1714f2aa52b15e8
unknown <niranjanaa.arun@gmail.com>
2024-09-21 15:16:00 -0500

    Lesson5 HW

--- features/steps/product_search.py
+++ features/steps/product_search.py
@@ -1,16 +1,35 @@
+from itertools import count
+from os.path import split
+
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from behave import given, when, then
 from time import sleep
 
 
 SEARCH_INPUT = (By.NAME, 'q')
 SEARCH_SUBMIT = (By.NAME, 'btnK')
+SELECTED_SIZE = (By.CSS_SELECTOR, "div[data-test='@web/VariationComponent'] li[data-io-i='1']")
+ALL_COLORS = (By.CSS_SELECTOR, "div[data-test='@web/VariationComponent']:nth-of-type(2) li")
+SELECTED_COLOR = (By.CSS_SELECTOR, "div[data-test='@web/VariationComponent']:nth-of-type(2)")
+SEARCH_WORD=(By.ID, 'search')
 
+LISTINGS= (By.CSS_SELECTOR, "div[data-test*='site-top-of-funnel/ProductCardWrapper']")
+PRODUCT_TITLE = (By.CSS_SELECTOR, "[data-test*='product-title']")
+PRODUCT_IMG= (By.CSS_SELECTOR, "picture[data-test='@web/ProductCard/ProductCardImage/primary'] img")
 
 @given('Open Google page')
 def open_google(context):
     context.driver.get('https://www.google.com/')
 
+@given('Open target product {product_id} page')
+def open_target_product(context, product_id):
+    context.driver.get(f'https://www.target.com/p/{product_id}')
+    sleep(8)
+
+# @given('Open Target page')
+# def open_target(context):
+#     context.driver.get('https://www.target.com/')
 
 @when('Input {search_word} into search field')
 def input_search(context, search_word):
@@ -25,8 +44,65 @@ def click_search_icon(context):
     context.driver.find_element(*SEARCH_SUBMIT).click()
     sleep(1)
 
+@when('Input {search_word} into search field1')
+def word(context, search_word):
+    search= context.driver.find_element(*SEARCH_WORD)
+    search.send_keys(search_word)
+    context.driver.find_element(By.CSS_SELECTOR, "[type='submit']").click()
 
-@then('Product results for {expected_result} are shown')
+@then('Product results for {search_word} are shown')
 def verify_found_results_text(context, search_word):
+
     assert search_word.lower() in context.driver.current_url.lower(), \
         f'Expected query not in {context.driver.current_url.lower()}'
+
+@then('Verify user can click through colors')
+def verify_colors(context):
+
+    context.driver.find_element(*SELECTED_SIZE).click()
+
+    expected_result = ['grey', 'navy/tan', 'stone/grey', 'white/sand/tan', 'dark khaki','white/navy/red']
+    actual_result = []
+
+    all_colors = context.driver.find_elements(*ALL_COLORS)
+    print(all_colors)
+
+    for color in all_colors:
+        print(color)
+        color.click()
+
+        selected_color = context.driver.find_element(*SELECTED_COLOR).text
+        print('Current color', selected_color)
+        selected_color = selected_color.split('\n')[1]
+        actual_result.append(selected_color)
+        print(actual_result)
+
+    assert actual_result == expected_result, f'{actual_result} is not {expected_result}'
+
+
+@then('Verify product name and product image')
+def verify_name(context):
+
+    context.driver.execute_script("window.scrollBy(0, 1000)","")
+    sleep(4)
+    context.driver.execute_script("window.scrollBy(0, 1000)","")
+    sleep(4)
+    context.driver.execute_script("window.scrollBy(0, 1000)", "")
+    sleep(4)
+    context.driver.execute_script("window.scrollBy(0, 1000)", "")
+    sleep(4)
+    context.driver.execute_script("window.scrollBy(0, 1000)", "")
+    sleep(4)
+    context.driver.execute_script("window.scrollBy(0, 1000)", "")
+    all_products= context.driver.find_elements(*LISTINGS)
+
+    for product in all_products:
+        title = product.find_element(*PRODUCT_TITLE).text
+        print(title)
+        # assert title, 'Product title not shown'
+        # print(title)
+        img = product.find_element(*PRODUCT_IMG)
+        print(img)
+
+
+
--- features/tests/product_search.feature
+++ features/tests/product_search.feature
@@ -4,9 +4,18 @@ Feature: Test Scenarios for Search functionality
     Given Open Google page
     When Input <search_word> into search field
     And Click on search icon
-    Then Product results for <expected_result> are shown
+    Then Product results for <search_word> are shown
     Examples:
-    |search_word  |expected_result
-    |Car          |Car
-    |Bike         |Bike
-    |MotorCycle   |Motorcycle
+    |search_word  |
+    |Car          |
+    |Bike         |
+    |MotorCycle   |
+
+  Scenario:  User can click on different colors
+    Given Open target product A-91511634 page
+    Then verify user can click through colors
+
+  Scenario: Verify that every product on Target search
+    Given Open Target page
+    When Input Tea into search field1
+    Then Verify product name and product image
